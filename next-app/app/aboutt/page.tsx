152bcfef6d152a7ce26e0c9ce015447021ee6379
dhreetijain04 <dhreetijain04@gmail.com>
2026-09-22 17:04:55 +0530

    rendering

---
+++ next-app/app/aboutt/page.tsx
@@ -0,0 +1,5 @@
+
+export default function Aboutt() {
+    console.log("server component");
+  return <h1> About Me</h1>
+}
---
+++ next-app/app/product/[id]/page.tsx
@@ -0,0 +1,13 @@
+
+export async function generateStaticParams(){
+    return [{id: "1"}, {id: "2"}, {id: "3"}]
+}
+export default async function ProductPage({
+    params,
+}:{
+    params : Promise<{id: string}>;
+}) {
+    const {id}= await params;
+
+  return <h1> product {id} details rendered </h1>
+}
